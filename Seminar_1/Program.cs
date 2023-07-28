b071d276b702133d5ff2e4c8c39e68ca182cc4ee
ThreeDokNait <nikitaaleksandrovichshiyatov@gmail.com>
2023-07-28 21:05:39 +0300

    add Закончили семинар

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,85 @@
+﻿Console.Clear();
+// Начало первой задачи на семинаре
+// Console.WriteLine("Get number, please");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int result = num*num;
+
+// Console.WriteLine(result);
+
+// Начало второй задачи на семинаре
+
+// Console.WriteLine("Get number 1, please");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Get number 2, please");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 == num2*num2)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No!");
+// }
+
+// Начало третьей задачи
+
+// Console.WriteLine("Get number , please");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1==1)
+// {
+//     Console.WriteLine("Monday");
+// }
+// else if(num1==2)
+// {
+//     Console.WriteLine("Tuesday");
+// }
+// else if(num1==3)
+// {
+//     Console.WriteLine("Wednesday");
+// }
+// else if(num1==4)
+// {
+//     Console.WriteLine("Thursday");
+// }
+// else if(num1==5)
+// {
+//     Console.WriteLine("Friday");
+// }
+// else if(num1==6)
+// {
+//     Console.WriteLine("Saturday");
+// }
+// else if(num1==7)
+// {
+//     Console.WriteLine("Sunday");
+// }
+// else
+// {
+//     Console.WriteLine("Do i look like a joke?");
+// }
+
+// Начало четвертой задачи
+
+// Console.WriteLine("Get number , please");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// int numNeg = num1*-1;
+
+// while(numNeg<=num1)
+// {
+//     Console.WriteLine(numNeg);
+//     numNeg=numNeg+1;
+// }
+//Начало пятой задачи
+
+Console.WriteLine("Get number, please");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int result= num1%10;
+
+Console.WriteLine(result);
